656e02d42949404db0f335ded799840f98bd2d5c
lganzzzo <lganzzzo@gmail.com>
2019-05-27 00:52:07 +0300

    Updated to latest oatpp APIs

--- main/src/App.cpp
+++ main/src/App.cpp
@@ -8,7 +8,6 @@
 
 #include "./controller/UserController.hpp"
 #include "./AppComponent.hpp"
-#include "./Logger.hpp"
 
 #include "oatpp-swagger/Controller.hpp"
 #include "oatpp/network/server/Server.hpp"
@@ -53,13 +52,10 @@ void run() {
  *  main
  */
 int main(int argc, const char * argv[]) {
-  
-  oatpp::base::Environment::setLogger(new Logger());
+
   oatpp::base::Environment::init();
 
   run();
-  
-  oatpp::base::Environment::setLogger(nullptr); ///< free Logger
   
   /* Print how much objects were created during app running, and what have left-probably leaked */
   /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
--- main/test/tests.cpp
+++ main/test/tests.cpp
@@ -10,18 +10,6 @@
 
 namespace {
 
-class Logger : public oatpp::base::Logger {
-private:
-  oatpp::concurrency::SpinLock m_lock;
-public:
-
-  void log(v_int32 priority, const std::string& tag, const std::string& message) override {
-    std::lock_guard<oatpp::concurrency::SpinLock> lock(m_lock);
-    std::cout << tag << ":" << message << "\n";
-  }
-
-};
-
 class Test : public oatpp::test::UnitTest {
 public:
   Test() : oatpp::test::UnitTest("MyTag")
@@ -45,12 +33,8 @@ void runTests() {
 int main() {
 
   oatpp::base::Environment::init();
-  oatpp::base::Environment::setLogger(new Logger());
 
   runTests();
-
-  oatpp::base::Environment::setLogger(nullptr);
-  oatpp::base::Environment::destroy();
 
   /* Print how much objects were created during app running, and what have left-probably leaked */
   /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
